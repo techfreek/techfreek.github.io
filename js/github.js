cbd81021163d94d08eb03bfb6c1beabfe21e0e82
Alex <alexgbahm@gmail.com>
2015-02-24 14:23:53 -0800

    Re-did how I create the text for my github activity tracker. It now supports all timeline events. I also made so it's pretty generic so techincally anyone could pick it up and tweak it for their use

--- js/github.js
+++ js/github.js
@@ -28,11 +28,18 @@ function countLanguageUse(repos) {
 		}
 		langs[repos[i].language].count += 1;
 
-		langs[repos[i].language].value += Math.sqrt(repos[i].size) / 15;
+		langs[repos[i].language].value += Math.sqrt(repos[i].size) / 10;
 
 		if(repos[i].language === "C++") {
 			//I had a really old repo with all the debug files still in it. Hence the decrement
 			langs[repos[i].language].value /= 10;
+		} else if(repos[i].language === "JavaScript") {
+			//These repos usually have a lot of images
+			langs[repos[i].language].value /= 1.1;
+		}
+
+		if(langs[repos[i].language].value > maxProjects) {
+			maxProjects = langs[repos[i].language].value;
 		}
 	}
 	return langs;
@@ -106,6 +113,28 @@ function findActivity(uname, callback) {
 }
 
 function addActivity(activities) {
+	var table = document.getElementById('github-activity-body');
+
+	for(var i = 0; i < 6 && i < activities.length; i++) {
+		var activity = activities[i];
+		var row = document.createElement('tr');
+		var td = document.createElement('td');
+
+		var body = gitActivityBody(activity);
+
+		td.innerHTML = body.actor +
+			body.action + body.target.link(body.link) + '<br>';
+		if(body.message) {
+			td.innerHTML += body.message;
+		}
+		
+		row.appendChild(td);
+		table.appendChild(row);
+	}
+
+}
+
+function gitActivityBody(activity) {
 	function convertAPIURI(uri) {
 		return uri.replace('api.', '').replace('repos/', '');
 	}
@@ -119,72 +148,108 @@ function addActivity(activities) {
 		return msg;
 	}
 
-	var table = document.getElementById('github-activity-body');
-
-	for(var i = 0; i < 6 && i < activities.length; i++) {
-		var activity = activities[i];
-		var row = document.createElement('tr');
-		var td = document.createElement('td');
-		
-		if(activity.type === "PushEvent") {
-			var refs = activity.payload.ref.split('/');
-			var url = convertAPIURI(activity.payload.commits[0].url);
-
-			td.innerHTML = activity.actor.login +
-				" pushed to " +
-				refs[refs.length - 1] +
-				" at " +
-				activity.repo.name.link(url) +
-				'<br>' +
-				trimMSG(activity.payload.commits[0].message);
-		} else if(activity.type === "IssueCommentEvent") {
-			var issueComment = activity.repo.name + '#' + activity.payload.issue.number;
-			var url = convertAPIURI(activity.payload.issue.html_url);
-
-			td.innerHTML = activity.actor.login +
-				" commented on issue " +
-				issueComment.link(url) +
-				'<br>' +
-				trimMSG(activity.payload.comment.body);
-		} else if(activity.type === "IssuesEvent") {
-			var issueComment = activity.repo.name + '#' + activity.payload.issue.number;
-			var url = convertAPIURI(activity.payload.issue.html_url);
-
-			td.innerHTML = activity.actor.login +
-				" opened issue " +
-				issueComment.link(url) +
-				'<br>' +
-				activity.payload.issue.title;
-
-		} else if(activity.type === "WatchEvent") {
-			td.innerHTML = activity.actor.login +
-				" starred " +
-				activity.repo.name.link('http://github.com/' + activity.repo.name);
-		} else if(activity.type === "CreateEvent") {
-			var url = convertAPIURI(activity.repo.url);
-			var name = activity.repo.name;
-			var link = name.link(url);
-			console.log("URL: " + url);
-			console.log("repo: " + name);
-			console.log("Link: " + link);
-
-			td.innerHTML = activity.actor.login +
-				" created repository " +
-				link;
-		} else if(activity.type === "PullRequestEvent") {
-			var url = activity.payload.pull_request.html_url;
-			var name = activity.repo.name + '#' + activity.payload.pull_request.number;
-
-			td.innerHTML = activity.actor.login + 
-				" merged pull request " +
-				name.link(url) +
-				'<br>' + 
-				trimMSG(activity.payload.pull_request.body);
-		}
-
-		row.appendChild(td);
-		table.appendChild(row);
+	function trimHash(hash) {
+		return hash.slice(0, 7);
 	}
 
-}
+	var data = {
+		actor: activity.actor.login,
+		message: '',
+		action: '',
+		target: '',
+		link: ''
+	}
 
+	if(activity.type === "CommitCommentEvent") {
+		data.action = "commented on commit";
+		data.link = 'http://github.com/' + activity.payload.repository.full_name;
+		data.target = activity.payload.repository.full_name
+		data.message = trimHash(activity.payload.comment.commit_id).link(activity.payload.comment.html_url) + ' ' +
+			 activity.payload.comment.body;
+
+	} else if(activity.type === "CreateEvent") {
+		data.link = activity.repo.url;
+		data.target = activity.repo.name;
+		data.action = "created repository";
+
+	} else if(activity.type === "DeleteEvent") {
+		data.action = "deleted " + action.payload.ref_type + " from";
+		data.target = activity.repository.full_name;
+		data.link = activity.repository.html_url;
+
+	} else if(activity.type === "ForkEvent") {
+		data.action = "forked";
+		data.target = action.payload.forkee.full_name;
+		data.link = action.payload.forkee.html_url;
+
+	} else if(activity.type === "GollumEvent") {
+		data.action = "updated " + action.payload.repository.full_name 
+			+ " wiki page";
+		data.target = action.payload.pages[0].page_name;
+		data.link = action.payload.pages[0].html_url;
+	} else if(activity.type === "IssueCommentEvent") {
+		var comment = activity.repo.name + "#" + activity.payload.issue.number;
+		
+		data.link = activity.payload.issue.html_url;
+		data.action = "commented on issue";
+		data.target = comment;
+		data.message = activity.payload.comment.body;
+
+	} else if(activity.type === "IssuesEvent") {
+		var issue = activity.repo.name + "#" + activity.payload.issue.number;
+
+		data.action = "opened issue";
+		data.target = issue;
+		data.link = activity.payload.issue.html_url;
+		data.message = activity.payload.issue.title;
+
+	} else if(activity.type === "MemberEvent") {
+		data.action = action.payload.action + " to";
+		data.target = action.repository.full_name;
+		data.link = action.repository.html_url;
+
+	} else if(activity.type === "PublicEvent") {
+		data.action = "open sourced";
+		data.target = activity.payload.repository.full_name;
+		data.link = activity.payload.repository.html_url;
+	
+	} else if(activity.type === "PullRequestEvent") {
+		data.action = " merged pull request ";
+		data.target = activity.repo.name + '#' + activity.payload.pull_request.number;
+		data.link = activity.payload.pull_request.html_url;
+		data.message = activity.payload.pull_request.body;
+
+	} else if(activity.type === "PullRequestReviewCommentEvent") {
+		data.action = "commented on pull request"
+		data.target = activity.payload.repo.full_name + '#' + 
+			activity.payload.pull_request.number;
+		data.link = activity.payload.comment.html_url;
+
+		data.message = activity.payload.comment.body;
+
+	} else if(activity.type === "PushEvent") {
+		var refs = activity.payload.ref.split('/'); 
+		data.action = "pushed to" + refs[refs.length - 1] + " at ";
+		data.link = activity.payload.commits[0].url;
+		data.target = activity.repo.name;
+		data.message = activity.payload.commits[0].message;
+
+	} else if(activity.type === "ReleaseEvent") {
+		data.action = action.payload.action + " " + action.payload.release.tag_name +
+			" of";
+		data.link = action.payload.release.html_url;
+		data.target = action.payload.repository.full_name;
+
+	} else if(activity.type === "WatchEvent") {
+		data.action = "starred";
+		data.link = 'http://github.com/' + activity.repo.name;
+		data.target = activity.repo.name;
+	}
+
+	//apply at the end so you I don't have to do in each activity type
+	data.action = " " + data.action + " ";
+	data.link = convertAPIURI(data.link);
+	data.message = trimMSG(data.message);
+
+	return data;
+}
